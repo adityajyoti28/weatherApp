bd120881b555843baf4c7385821ba2fc8049551b
Aditya Jyoti <83084415+adityajyoti28@users.noreply.github.com>
2024-04-11 20:58:09 +0530

    added api keys

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "81c4e8934499bafd8e157e086bd7354f",
   base: "https://api.openweathermap.org/data/2.5/",
 };
